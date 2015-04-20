9db8fd95f29565e41f358d70a279e5751fd87e49
florianhidayat <florian1846@gmail.com>
2015-04-20 19:34:51 +0800

    Add reveal layout in discover fragment

--- app/src/main/java/com/molaja/android/fragment/discover/DiscoverFragment.java
+++ app/src/main/java/com/molaja/android/fragment/discover/DiscoverFragment.java
@@ -1,16 +1,21 @@
 package com.molaja.android.fragment.discover;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentTabHost;
-import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewAnimationUtils;
 import android.view.ViewGroup;
+import android.widget.EditText;
 
 import com.molaja.android.R;
 import com.molaja.android.adapter.DiscoverTabAdapter;
+import com.molaja.android.widget.RevealLayout;
 import com.ogaclejapan.smarttablayout.SmartTabLayout;
 
 /**
@@ -20,41 +25,43 @@ public class DiscoverFragment extends Fragment {
     public static final String TAB_1_TAG = "ITEMS";
     public static final String TAB_2_TAG = "SHOPS";
     public static final String TAB_3_TAG = "CATEGORIES";
-    FragmentTabHost mTabHost;
+
+    //views
     ViewPager viewPager;
+    RevealLayout revealLayout;
+    RecyclerView searchView;
+    EditText searchBox;
+
     DiscoverTabAdapter mAdapter;
-    PagerTabStrip pagerTabStrip;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View fragmentView = inflater.inflate(R.layout.fragment_discover, container, false);
-        initPager(fragmentView);
+        initViews(fragmentView);
         return fragmentView;
     }
 
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-    }
-
-    public void initPager(View v) {
+    public void initViews(View v) {
         viewPager = (ViewPager) v.findViewById(R.id.pager);
+        searchBox = (EditText) v.findViewById(R.id.search_box);
+        searchView = (RecyclerView) v.findViewById(R.id.search_view);
+        revealLayout = (RevealLayout) v.findViewById(R.id.discover_reveal_layout);
+
         mAdapter = new DiscoverTabAdapter(getChildFragmentManager());
         viewPager.setAdapter(mAdapter);
         viewPager.setCurrentItem(1);
-        /*pagerTabStrip = (PagerTabStrip) v.findViewById(R.id.pager_tabstrip);
-        //pagerTabStrip.setBackgroundResource(R.drawable.tab_indicator);
-        pagerTabStrip.setTextSize(TypedValue.COMPLEX_UNIT_SP,16f);
-        pagerTabStrip.setTabIndicatorColorResource(R.color.White);
 
-        for (int i = 0; i < pagerTabStrip.getChildCount(); ++i) {
-            View nextChild = pagerTabStrip.getChildAt(i);
-            if (nextChild instanceof TextView) {
-                TextView textViewToConvert = (TextView) nextChild;
-                textViewToConvert.setTypeface(Typeface.SANS_SERIF);
-            }
-        }*/
+        //init search view
+        searchView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
         SmartTabLayout viewPagerTab = (SmartTabLayout) v.findViewById(R.id.viewpager_tab);
         viewPagerTab.setViewPager(viewPager);
+
+        searchBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                revealLayout.next();
+            }
+        });
     }
 }
